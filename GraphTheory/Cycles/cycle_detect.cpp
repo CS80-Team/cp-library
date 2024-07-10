440432f39f0393a410bf97443a5c1c1c6ee9c396
YahiaHelal <yahiarashwan348@gmail.com>
2024-07-10 11:19:12 +0300

    solve implementation issue

--- GraphTheory/Cycles/cycle_detect.cpp
+++ GraphTheory/Cycles/cycle_detect.cpp
@@ -1,17 +1,15 @@
 
 // return true with number of nodes in the cycle, either odd cycle or even
-bool cycle_detection(unordered_map<int, vector<int>> &graph, int source, unordered_map<int,bool> vis, int c){
-    if(vis[source]){
-        return true;
-    }
-    vis[source] = true;
-    for(int v: graph[source]){
-        if(v != source){
-            c++;
-            if(cycle_detection(graph, source, vis,c)){
-                return true; 
-            }
-        }
-    }
-    return false;
+bool cycle_detection(unordered_map<int, vector<int>> &graph, int source, int par, unordered_map<int,bool> vis, int c){
+    if(vis[source]) return true;
+
+   vis[source] = true;
+
+   for(int v: graph[source]){
+    if(v != par){
+        c++;
+        if(dfs(graph,v, source, vis, c)) return true;
+    }   
+   }
+   return false;
 }
