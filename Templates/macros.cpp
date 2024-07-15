d7e120b35e08d04b1a1343160514933bf757ff9d
ahmed-ashraff <ahmedadhraf03@gmail.com>
2024-07-15 17:14:08 +0300

    macros

---
+++ Templates/macros.cpp
@@ -0,0 +1,31 @@
+#define getBit(n, k) (n >> k)
+#define ON(n, idx) (n | (1ll << idx))
+#define OFF(n, idx) (n & ~(1ll << idx))
+#define toggle(n, idx) ((n) ^ (1ll<<(idx)))
+#define gray(n) (n ^ (n >> 1))
+#define bitCount(x) (__builtin_popcountll(x))
+#define uniq(x) x.resize(unique(x.begin(), x.end())-x.begin());
+
+#define angle(a) (atan2((a).imag(), (a).real()))
+//#define vec(a, b) ((b)-(a))
+#define same(v1, v2) (dp(vec(v1,v2),vec(v1,v2)) < EPS)
+#define dotProduct(a, b) ((conj(a)*(b)).real())    // a*b cos(T), if zero -> prep
+#define crossProduct(a, b) ((conj(a)*(b)).imag())    // a*b sin(T), if zero -> parallel
+//#define length(a) (hypot((a).imag(), (a).real()))
+#define normalize(a) ((a)/length(a))
+#define rotateO(v, ang) ((v)*exp(point(0,ang)))
+#define rotateA(p, ang, about) (rotateO(vec(about,p),ang)+about)
+#define reflectO(v, m) (conj((v)/(m))*(m))
+#define ceil_i(a, b) (((ll)(a)+(ll)(b-1))/(ll)(b))
+#define floor_i(a, b) (a/b)
+#define round_i(a, b) ((a+(b/2))/b) // if a>0
+#define round_m(a, b) ((a-(b/2))/b) // if a<0
+#define round_multiple(n, m) round_i(n,m)*m // round to multiple if specified element
+
+const double PI = acos(-1.0);
+
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+
+int dx[8] = {1, 1, -1, -1, 2, 2, -2, -2};
+int dy[8] = {2, -2, 2, -2, 1, -1, 1, -1};
