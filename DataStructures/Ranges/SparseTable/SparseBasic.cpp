1a5e88eeb1e5d255354a46c087f714c20a536923
Omar Muhammad <omarmgaber37@gmail.com>
2024-07-16 14:30:36 +0300

    Sparse Table

---
+++ DataStructures/Ranges/SparseTable/SparseBasic.cpp
@@ -0,0 +1,56 @@
+// storing the index also
+struct SNode {
+    int val;
+    int index;
+};
+
+class SparseTable {
+private:
+    vector<vector<SNode>> table;
+
+    function<SNode(const SNode&, const SNode&)> merge;
+
+    static SNode StaticMerge(const SNode& a, const SNode& b) {
+        return a.val < b.val ? a : b;
+    }
+
+public:
+    explicit SparseTable(const vector<int>& arr, const function<SNode(const SNode&, const SNode&)>& mergeFunc = StaticMerge) {
+        int n = static_cast<int>(arr.size());
+        int log_n = static_cast<int>(log2(n)) + 1;
+        this->merge = mergeFunc;
+
+        table.resize(n, vector<SNode>(log_n));
+
+        for (int i = 0; i < n; i++) {
+            table[i][0] = {arr[i], i};
+        }
+
+        for (int j = 1; (1 << j) <= n; j++) {
+            for (int i = 0; i + (1 << j) <= n; i++) {
+                table[i][j] = mergeFunc(table[i][j - 1], table[i + (1 << (j - 1))][j - 1]);
+            }
+        }
+    }
+
+    SNode query(int left, int right) {
+        int j = static_cast<int>(log2(right - left + 1));
+        return merge(table[left][j], table[right - (1 << j) + 1][j]);
+    }
+};
+
+
+int main(void) {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (auto& element : arr) cin >> element;
+
+    SparseTable minSt(arr, [](const SNode& a, const SNode& b) -> SNode {
+        return a.val < b.val ? a : b;
+    });
+
+    SparseTable maxSt(arr, [](const SNode& a, const SNode& b) -> SNode {
+        return a.val > b.val ? a : b;
+    });
+}
