46c49d4c0f1df79cd310e15cff0d3f9fd4d34594
Omar Muhammad <omarmgaber37@gmail.com>
2024-07-17 21:02:30 +0300

    Added GCC compiler optimizations pragmas, riased templete section to be the first section

---
+++ Templates/gcc.cpp
@@ -0,0 +1,8 @@
+// Ref: USACO guide
+// will make GCC auto-vectorize for loops and optimizes floating points better (assumes associativity and turns off denormals).
+#pragma GCC optimize ("Ofast")
+// can double performance of vectorized code, but causes crashes on old machines.
+#pragma GCC target ("avx,avx2")
+
+// slows down run time but throws a Runtime Error if an overflow occured
+#pramga GCC optimize("trapv")
