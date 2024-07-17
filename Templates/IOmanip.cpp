f4208fbdcfb3c2e3d50cd0d0505d01abe11d897d
Omar Muhammad <omarmgaber37@gmail.com>
2024-07-17 21:08:05 +0300

    Added main IO manipulation setup

---
+++ Templates/IOmanip.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+
+#define fastIO             \
+  ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
