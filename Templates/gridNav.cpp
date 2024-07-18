8618c2b641553ff639b9f17de331f9b8b1b8f0a7
Omar Muhammad <omarmgaber37@gmail.com>
2024-07-18 10:31:42 +0300

    Grid Navigation arrays

---
+++ Templates/gridNav.cpp
@@ -0,0 +1,16 @@
+// knight moves on a chess board
+int dx[] = { -2, -1, 1, 2, -2, -1, 1, 2 };
+int dy[] = { -1, -2, -2, -1, 1, 2, 2, 1 };
+
+// Grid up, down, right, left (Moves for Chess Rook)
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+
+// Grid cell all neighbours
+const int dx[8] = {1, 0, -1, 0, 1, 1, -1, -1}
+const int dy[8] = {0, 1, 0, -1, -1, 1, -1, 1};
+
+
+// Grid Diagonal (Moves for Chess Bishop)
+int dx[] = {1, 1, -1, -1};
+int dy[] = {1, -1, 1, -1};
